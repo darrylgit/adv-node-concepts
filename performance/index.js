bbe21cee903bdd8248f9070b24c1945a9e83fac6
darrylgit <darrylsmithup@gmail.com>
2020-04-19 14:06:51 -0400

    Set up clustering

--- performance/index.js
+++ performance/index.js
@@ -1,8 +1,23 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-app.get("/", (req, res) => {
-  res.send("Hi there");
-});
+/// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed in again but in child mode
+  cluster.fork();
+} else {
+  // I'm a child, I'm going to act like a server and do nothing else
+  const express = require("express");
+  const app = express();
 
-app.listen(3000);
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doWork(5000);
+    res.send("Hi there");
+  });
+
+  app.listen(3000);
+}
