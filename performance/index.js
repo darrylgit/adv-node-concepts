69ea3695e060629bcd9f6b03ee3476b7728759e6
darrylgit <darrylsmithup@gmail.com>
2020-04-19 21:14:28 -0400

    Set up manual clusters

--- performance/index.js
+++ performance/index.js
@@ -1,25 +1,23 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
 
 /// Is the file being executed in master mode?
 if (cluster.isMaster) {
   // Cause index.js to be executed in again but in child mode
   cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
-  // cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
 } else {
   // I'm a child, I'm going to act like a server and do nothing else
   const express = require("express");
   const app = express();
-
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
+  const crypto = require("crypto");
 
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hi there");
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hi there");
+    });
   });
 
   app.get("/fast", (req, res) => {
