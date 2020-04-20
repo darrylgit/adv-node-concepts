c6ffd0f76893087f3e8b3a359deec987e427bf89
darrylgit <darrylsmithup@gmail.com>
2020-04-19 21:18:09 -0400

    Configure server for PM2

--- performance/index.js
+++ performance/index.js
@@ -1,28 +1,16 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require("cluster");
+// I'm a child, I'm going to act like a server and do nothing else
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
 
-/// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed in again but in child mode
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-  cluster.fork();
-} else {
-  // I'm a child, I'm going to act like a server and do nothing else
-  const express = require("express");
-  const app = express();
-  const crypto = require("crypto");
-
-  app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("Hi there");
-    });
+app.get("/", (req, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("Hi there");
   });
+});
 
-  app.get("/fast", (req, res) => {
-    res.send("This was fast");
-  });
+app.get("/fast", (req, res) => {
+  res.send("This was fast");
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
